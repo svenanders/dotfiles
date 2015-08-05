509d9419baa43e859c084ec71dff0117f0303317
Sven Anders Robbestad <robbestad@gmail.com>
2015-08-05 08:20:04 +0200

    Added osx-for-hackers

--- osx-for-hackers.sh
+++ osx-for-hackers.sh
@@ -187,8 +187,8 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 echo ""
 echo "Setting trackpad & mouse speed to a reasonable number"
-defaults write -g com.apple.trackpad.scaling 5
-defaults write -g com.apple.mouse.scaling 5
+defaults write -g com.apple.trackpad.scaling 7
+defaults write -g com.apple.mouse.scaling 7
 
 echo ""
 echo "Turn off keyboard illumination when computer is not used for 5 minutes"
